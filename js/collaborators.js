88fc6f11fc065ca0e275b3565e997eac27895bef
stely <129224257+sstely@users.noreply.github.com>
2023-11-18 15:37:05 +0200

    added part 1 of speak-collab UI

---
+++ js/collaborators.js
@@ -0,0 +1,26 @@
+const newCollabBtn = document.getElementById("addCollab");
+const addCollabBtn = document.getElementById("addBtn");
+
+const backBtn = document.getElementById("backControlPanel");
+
+const addPopup = document.getElementById("addPopup");
+const closePopup = document.getElementById("closePopup");
+
+newCollabBtn.addEventListener('click', () => {
+    addPopup.classList.add("open-popup");
+})
+
+addCollabBtn.addEventListener('click', () => {
+    const form = document.querySelector("form");
+    form.reset();
+
+    addPopup.classList.remove("open-popup");
+})
+
+backBtn.addEventListener('click', () => {
+    window.location.href = 'controlPanel.html';
+})
+
+closePopup.addEventListener('click', () => {
+    addPopup.classList.remove("open-popup");
+})
